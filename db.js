b8dc706a8a3e2e7d9de4080e2d3831ffd72ddcf4
Adithya Nair <adtya.nair@gmail.com>
2021-02-22 22:59:51 +0530

    add db name to connection url

--- db.js
+++ db.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const DB_URI = process.env["DB_URI"];
 mongoose.connect(
   DB_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  {
+    auth: { authSource: "admin" },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
   (err) => {
     if (err) {
       console.log(err);
