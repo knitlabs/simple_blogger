7b1aa707c7e96968811c0c1d48912c1952f8188c
Adithya Nair <adtya.nair@gmail.com>
2021-02-22 23:10:35 +0530

    add dummy css file

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/public", express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
