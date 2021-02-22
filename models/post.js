3a3e0e0f744bec91ea84450eeebb4ebb0c132f69
Adithya Nair <adtya.nair@gmail.com>
2021-02-22 23:00:05 +0530

    add posts model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require("../db");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+  content: { type: String },
+});
+const Post = mongoose.model("post", postSchema);
+
+module.exports = Post;
