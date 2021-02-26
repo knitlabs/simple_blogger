74842240af5f2da9326fe08b32c2228af5f6dddd
Adithya Nair <git@adtya.xyz>
2021-02-26 22:54:20 +0530

    disable helmet for now

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-app.use(helmet());
+// app.use(helmet({}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
